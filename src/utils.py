0dfd5eebd949ceedd1c346d36601dc0a23cbb439
AniRoy10 <anirjit.datta.roy.98@gmail.com>
2024-06-18 02:31:22 +0530

    logging and exception

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):    #sys knows the error_details , it has it
+    _,_,exc_tb=error_detail.exc_info()              # we are not interseted in first two terms
+    file_name=exc_tb.tb_frame.f_code.co_filename    #as per python exception handling doucmentation , thus we can get filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))         #error message
+
+    return error_message
+
+    
+
+class CustomException(Exception):           #inheriting parent exception
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)                 #inheriting __init__ from Exception
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)  #we are calling the function from error message
+    
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
